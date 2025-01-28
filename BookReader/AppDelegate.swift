37e48a308f3d28a6b4ee1c52709c05ad8a75dbd5
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2025-01-28 20:44:09 +0300

    added main LibraryViewController

--- BookReader/AppDelegate.swift
+++ BookReader/AppDelegate.swift
@@ -4,12 +4,16 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Properties
     var window: UIWindow?
+    var navigationController: UINavigationController?
     
     // MARK: - Methods
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
+        navigationController = UINavigationController(rootViewController: LibraryViewController())
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         
         return true
---
+++ BookReader/LibraryViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// MARK: - LibraryViewController
+final class LibraryViewController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Input view
+    override func loadView() {
+        self.view = LibraryInputView()
+    }
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigationItem()
+        
+        // setup mock fb2 book to Files on the simulator
+        FManager.shared.writeMockFB2Book()
+    }
+    
+    // MARK: - Private Methods
+    private func setupNavigationItem() {
+        self.navigationItem.title = "Моя библиотека"
+        self.navigationItem.largeTitleDisplayMode = .always
+    }
+}
