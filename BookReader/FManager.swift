9164cf7883ec9883cf4be1d56bfccd67b521891d
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2025-02-02 23:50:01 +0300

    edited methods for setting mock book

--- BookReader/FManager.swift
+++ BookReader/FManager.swift
@@ -13,13 +13,24 @@ final class FManager {
     private init() {}
     
     // MARK: - Methods Mock
-    func getMockFB2BookPath() -> String? {
+    func getMockFB2BookData() -> Data? {
+        if FileManager.default.fileExists(atPath: self.getMockFB2BookPath()) {
+            if let data = try? Data(contentsOf: URL(string: self.getMockFB2BookPath())!) {
+                return data
+            }
+        }
+        return nil
+    }
+    func getMockFB2BookPath() -> String {
+        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.path().appending("book.fb2")
+    }
+    func getMockFB2BookBundlePath() -> String? {
         Bundle.main.path(forResource: "book", ofType: "fb2")
     }
     
     func writeMockFB2Book() {
         let destinationURL = self.documentsPath.appending("book.fb2")
-        try? FileManager.default.copyItem(atPath: self.getMockFB2BookPath()!, toPath: destinationURL)
+        try? FileManager.default.copyItem(atPath: self.getMockFB2BookPath(), toPath: destinationURL)
     }
     
 }
