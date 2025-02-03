35d802b41899be6929075a4faff43a794d8d8614
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2025-02-03 23:13:59 +0300

    added BookView class

--- BookReader/BookView.swift
+++ BookReader/BookView.swift
@@ -1,8 +1,28 @@
-//
-//  BookView.swift
-//  BookReader
-//
-//  Created by Александр Павлицкий on 30.01.2025.
-//
+import UIKit
 
-import Foundation
+// MARK: - BookView
+final class BookView: UIView {
+    // MARK: - Init
+    init() {
+        super.init(frame: .zero)
+        setupLayout()
+        setupBorder()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    private func setupLayout() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.widthAnchor.constraint(equalToConstant: 100),
+            self.heightAnchor.constraint(equalToConstant: 250)
+        ])
+    }
+    private func setupBorder() {
+        self.layer.borderWidth = 1.5
+        self.layer.borderColor = UIColor.black.cgColor
+    }
+}
