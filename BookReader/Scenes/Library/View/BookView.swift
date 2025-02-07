d75d2f97a5c99e3d439b4c8b62444cbd578c73b4
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2025-02-08 00:57:43 +0300

    improved UI displaying for BookView

--- BookReader/Scenes/Library/View/BookView.swift
+++ BookReader/Scenes/Library/View/BookView.swift
@@ -1,17 +1,11 @@
 import UIKit
 import Foundation
 
-// MARK: - BookProgress
-enum BookProgress {
-    case value(Int)
-    case done
-}
-
 // MARK: - BookView
-final class BookView: UIView {
+final class BookView: UICollectionViewCell {
     // MARK: - Properties
-    private let progressImageViewWidthPercent = 6.1068
-    private let progressImageViewHeightPercent = 2.8196
+    private let progressViewWidthPercent = 6.1068
+    private let progressViewHeightPercent = 2.8196
     
     private let coverImageViewWidthPercent = 38.1679
     private let coverImageViewHeightPercent = 24.9433
@@ -20,24 +14,44 @@ final class BookView: UIView {
     private lazy var coverImageView: UIImageView = {
         let imageView = UIImageView()
         
-        //constraints
-        let width = CGFloat(round(UIScreen.main.bounds.width * coverImageViewWidthPercent / 100))
-        let height = CGFloat(round(UIScreen.main.bounds.width * coverImageViewHeightPercent / 100))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         
         return imageView
     }()
     
-    private lazy var progressImageView: UIImageView = {
-        let imageView = UIImageView()
+    private lazy var progressView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
         
         //constraints
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        let width = CGFloat(round(UIScreen.main.bounds.width * progressImageViewWidthPercent / 100))
+        view.translatesAutoresizingMaskIntoConstraints = false
+        let width = CGFloat(round(UIScreen.main.bounds.width * progressViewWidthPercent / 100))
         let height = width
         NSLayoutConstraint.activate([
-            imageView.widthAnchor.constraint(equalToConstant: width),
-            imageView.heightAnchor.constraint(equalToConstant: height)
+            view.widthAnchor.constraint(equalToConstant: width),
+            view.heightAnchor.constraint(equalToConstant: height)
         ])
+        
+        return view
+    }()
+    
+    private lazy var progressTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 9, weight: .semibold)
+        label.textColor = .black
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private lazy var progressDoneImageView: UIImageView = {
+        let imageConfiguration = UIImage.SymbolConfiguration(pointSize: 18, weight: .semibold)
+        let image = UIImage(systemName: "checkmark", withConfiguration: imageConfiguration)
+        let imageView = UIImageView(image: image)
+        imageView.tintColor = .appGreen
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         
         return imageView
     }()
@@ -46,30 +60,80 @@ final class BookView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        self.progressImageView.layer.cornerRadius = self.progressImageView.frame.width / 2
+        self.progressView.layer.cornerRadius = self.progressView.frame.width / 2
+        
     }
     
     // MARK: - Init
     init() {
         super.init(frame: .zero)
-        setupLayout()
-        setupBorder()
+        configureCell()
+        configureCoverImageViewLayout()
+        configureProgressViewLayout()
+        configureProgressTitleLabelLayout()
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Methods
+    func setupUI(cover: UIImage, percent: Int) {
+        self.coverImageView.image = cover
+        
+        if percent == 100 {
+            self.replaceView(progressTitleLabel, with: progressDoneImageView, in: progressView)
+        } else {
+            self.progressTitleLabel.text = "\(percent)%"
+        }
+        
+    }
+    
     // MARK: - Private Methods
-    private func setupLayout() {
-        self.translatesAutoresizingMaskIntoConstraints = false
+    private func configureCell() {
+        self.layer.cornerRadius = 15
+        self.clipsToBounds = true
+    }
+    
+    private func configureCoverImageViewLayout() {
+        self.contentView.addSubview(coverImageView)
         NSLayoutConstraint.activate([
-            self.widthAnchor.constraint(equalToConstant: 100),
-            self.heightAnchor.constraint(equalToConstant: 250)
+            coverImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            coverImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            coverImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            coverImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
         ])
     }
-    private func setupBorder() {
-        self.layer.borderWidth = 1.5
-        self.layer.borderColor = UIColor.black.cgColor
+    
+    private func configureProgressViewLayout() {
+        self.contentView.addSubview(progressView)
+        NSLayoutConstraint.activate([
+            progressView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 4),
+            progressView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 4)
+        ])
     }
+    
+    private func configureProgressTitleLabelLayout() {
+        self.progressView.addSubview(progressTitleLabel)
+        NSLayoutConstraint.activate([
+            progressTitleLabel.centerXAnchor.constraint(equalTo: self.progressView.centerXAnchor),
+            progressTitleLabel.centerYAnchor.constraint(equalTo: self.progressView.centerYAnchor)
+        ])
+    }
+    
+    private func replaceView(_ oldView: UIView, with newView: UIView, in parentView: UIView) {
+        UIView.transition(with: parentView, duration: 0.3, options: .transitionCrossDissolve, animations: {
+            oldView.removeFromSuperview()
+        }, completion: { [weak self] _ in
+            guard let self = self else { return }
+            parentView.addSubview(newView)
+            
+            NSLayoutConstraint.activate([
+                newView.centerXAnchor.constraint(equalTo: self.progressView.centerXAnchor),
+                newView.centerYAnchor.constraint(equalTo: self.progressView.centerYAnchor)
+            ])
+        })
+    }
+
 }
