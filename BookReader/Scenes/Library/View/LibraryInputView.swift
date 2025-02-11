04a44b593d31d4c01ec1052ed44119e85a6e44ab
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2025-02-11 22:25:08 +0300

    added searchbar and sort button to LibraryInputView

--- BookReader/Scenes/Library/View/LibraryInputView.swift
+++ BookReader/Scenes/Library/View/LibraryInputView.swift
@@ -2,43 +2,63 @@ import UIKit
 
 // MARK: - LibraryInputView
 final class LibraryInputView: UIView {
+    // MARK: - Properties
+    let trailingInset: CGFloat = -20
     // MARK: - GUI
-    lazy var addFileButton: UIButton = {
+    private lazy var sortButton: UIButton = {
         let button = UIButton()
-        
-        button.backgroundColor = .darkGray
-        
-        //constraints
         button.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            button.widthAnchor.constraint(equalToConstant: 100),
-            button.heightAnchor.constraint(equalToConstant: 100)
-        ])
         
+        let imageConfiguration = UIImage.SymbolConfiguration(pointSize: 28, weight: .regular)
+        let image = UIImage(systemName: "line.3.horizontal.decrease.circle", withConfiguration: imageConfiguration)
+        
+        button.setImage(image, for: .normal)
+        button.tintColor = .appGray
         return button
+    }()
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Поиск"
+        searchBar.showsBookmarkButton = true
+        searchBar.setImage(UIImage(systemName: "mic.fill"), for: .bookmark, state: [])
+        searchBar.setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+        
+        // constraints for searchBar
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        return searchBar
     }()
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         setupView()
-        setupAddFileButtonLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Methods
+    func setupSearchBarLayout() {
+        self.addSubview(searchBar)
+        NSLayoutConstraint.activate([
+            searchBar.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 11),
+            searchBar.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -63),
+            searchBar.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor)
+        ])
+    }
+    
+    func setupSortButtonLayout() {
+        self.addSubview(sortButton)
+        NSLayoutConstraint.activate([
+            sortButton.topAnchor.constraint(equalTo: self.searchBar.searchTextField.topAnchor),
+            sortButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: trailingInset)
+        ])
+    }
+    
     // MARK: - Private Methods
     private func setupView() {
         self.backgroundColor = .white
-    }
-    
-    private func setupAddFileButtonLayout() {
-        self.addSubview(addFileButton)
-        NSLayoutConstraint.activate([
-            addFileButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            addFileButton.centerYAnchor.constraint(equalTo: self.centerYAnchor)
-        ])
     }
 }
