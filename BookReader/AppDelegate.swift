b8d3513a5aa8e3d9d9ad5a7394359ae0a930f406
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2025-02-16 00:30:45 +0300

    removed useless navigation controller in appDelegate

--- BookReader/AppDelegate.swift
+++ BookReader/AppDelegate.swift
@@ -9,9 +9,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Methods
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
-        navigationController = UINavigationController(rootViewController: LibraryViewController())
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = MainTabBarController()
         window?.makeKeyAndVisible()
