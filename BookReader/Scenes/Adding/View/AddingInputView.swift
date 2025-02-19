1b53d3a71fb80d13cdd4cd333a580d23edfc6397
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2025-02-19 13:22:34 +0300

    added AddingInputView for AddingViewController

---
+++ BookReader/Scenes/Adding/View/AddingInputView.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class AddingInputView: UIView {
+    // MARK: - GUI
+    
+    
+    // MARK: - Private Methods
+    private func setupView() {
+        self.backgroundColor = .systemGray5
+    }
+}
