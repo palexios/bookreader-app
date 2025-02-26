626459e3f90fb8f8fb7227d01c8fdd761fc6643b
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2025-02-27 01:34:53 +0300

    added AddingViewController and implemented the inputView

--- BookReader/Scenes/Adding/View/AddingViewController.swift
+++ BookReader/Scenes/Adding/View/AddingViewController.swift
@@ -3,7 +3,12 @@ import UIKit
 // MARK: - AddingViewController
 final class AddingViewController: UIViewController {
     // MARK: - Life Cycle
+    override func loadView() {
+        self.view = AddingInputView()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 }
