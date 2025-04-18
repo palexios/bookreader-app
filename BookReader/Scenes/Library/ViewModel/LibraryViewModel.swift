f96682b30d1cfdbb3eb53b81c329d0aee4867ef2
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2025-04-18 13:43:28 +0300

    refactored code

--- BookReader/Scenes/Library/ViewModel/LibraryViewModel.swift
+++ BookReader/Scenes/Library/ViewModel/LibraryViewModel.swift
@@ -1,13 +1,19 @@
 import UIKit
 import Foundation
+import CoreData
 
 // MARK: LibraryViewModel
 final class LibraryViewModel {
     // MARK: - Properties
     var selectedCell: BookCollectionViewCell?
-    var mockBooks: [Book] {
-        let cover1 = UIImage.mi2.jpegData(compressionQuality: 1.0)
-        let book1 = Book(cover: cover1, name: "Мартин Иден", author: "Джек Лондон", status: 67, genre: .none)
-        return [book1]
+    private var coreData = CoreDataManager.shared
+    
+    var books: [BookModel] {
+        return getBooks()
+    }
+    
+    private func getBooks() -> [BookModel] {
+        let books = self.coreData.fetchBooks()
+        return books.map { $0.toBookModel() }
     }
 }
